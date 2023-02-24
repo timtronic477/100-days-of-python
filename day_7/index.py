13c5a6376908b7f9e333000eeabb6a59ecb36cb3
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2023-02-23 17:35:09 -0800

    day 7 done

--- day_7/index.py
+++ day_7/index.py
@@ -1,21 +1,47 @@
 import random
+from hangman_art import *
+from hangman_words import *
 #Step 1 
 
-word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+print(logo)
 chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Guess a letter: ").lower()
+word_length = len(chosen_word)
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+display = []
+for i in range(word_length):
+    display.append("_")
+print(display)
 
-for i in chosen_word:
-    if i == guess:
-        print("True")
-    else:
-        print("False")
+lives = 6
+game_over = False
+wrong_guesses = []
+
+while game_over == False:
+    guess = input("Guess a letter: ").lower()
     
+    if guess in display:
+        print("You already guessed that, try something else")
+    for i in range(word_length):
+        letter = chosen_word[i]
+        if letter == guess:
+            display[i] = guess
+        
+    if guess not in chosen_word:
+        print (f"You guessed the letter {guess}, you lose a life")
+        wrong_guesses.append(guess)
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("You lost")
+
+    print(display)   
+    if "_" not in display:
+        game_over = True
+        print("You win")           
+        
+    print(stages[lives]) 
