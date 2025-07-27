64699fef9dbb13b6714c1081a8628dab64315691
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2025-07-27 00:01:31 -0700

    adjusted most of it

--- day+11/main.py
+++ day+11/main.py
@@ -8,12 +8,15 @@ def r():
     return random.randint(0, 12)
 
 def deal():
-    p.append(deck[r()])
+    p.append(deck[12])
     p.append(deck[r()])
     d.append(deck[r()])
 
 def hit():
     p.append(deck[r()])
+
+def d_hit():
+    d.append(deck[r()])
 
 def board():
     print(f"Your cards: {p}, current score: {sum(p)}")
@@ -23,15 +26,56 @@ def bust():
     print(f"Computer's final hand: {d}, final score {sum(d)}.")
     print("You went over, you lose :(")
 
+def d_bust():
+    print(f"Your final hand: {p}, final score: {sum(p)}")
+    print(f"Computer's final hand: {d}, final score {sum(d)}.")
+    print("Computer went over, you win :)")
+
+def tie():
+    print(f"Your final hand: {p}, final score: {sum(p)}")
+    print(f"Computer's final hand: {d}, final score {sum(d)}.")
+    print("You scored the same, Tie")
+
+def win():
+    print(f"Your final hand: {p}, final score: {sum(p)}")
+    print(f"Computer's final hand: {d}, final score {sum(d)}.")
+    print("You scored the higher, you win :)")
+
+def lose():
+    print(f"Your final hand: {p}, final score: {sum(p)}")
+    print(f"Computer's final hand: {d}, final score {sum(d)}.")
+    print("Computer scored the higher, you lose :(")
+
 def play():
     pl = input("you want to play a game of Blackjack? Type 'y' or 'n': ")
     if pl == 'y':
         blackjack()
     else:
         return
+
 def clear():
     p.clear()
     d.clear()
+
+def check():
+    if sum(p) > 21 and 11 in p:
+       p[p.index(11)] = 1
+
+def d_check():
+    if sum(d) > 17 and 11 in p:
+       d[d.index(11)] = 1
+
+def soft_17():
+    while sum(d) < 17:
+        d_hit()
+
+def compare():
+    if sum(d) == sum(p):
+        tie()
+    if sum(p) > sum(d):
+        win()
+    if sum(p) < sum(d):
+        lose()
 
 def blackjack():
     money_on_table = True
@@ -42,6 +86,7 @@ def blackjack():
         h = input("Type 'y' to get another card, type 'n' to pass: ")
         if h == 'y':
             hit()
+            check()
             if sum(p) > 21:
                 bust()
                 money_on_table = False
@@ -49,4 +94,18 @@ def blackjack():
                 play()
             else:
                 board()
+        else:
+            soft_17()
+            if sum(d) > 21:
+                d_bust()
+                money_on_table = False
+                clear()
+                play()
+            else:
+                compare()
+                money_on_table = False
+                clear()
+                play()
+
+
 blackjack()
