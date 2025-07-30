51aaaabe8815b6a5e9a53f1d4c6a4626df75c58d
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2025-07-29 23:28:55 -0700

    finished day 13

--- day_13/main.py
+++ day_13/main.py
@@ -2,3 +2,15 @@ def my_function():
     for i in range(1, 21):
         if i == 20:
             print("You got it")
+
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
